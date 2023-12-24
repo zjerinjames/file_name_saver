e0906061fb74732622c3d6f8e0ebf4aba40ceb87
zjerinjames <zjerinjames@gmail.com>
2023-12-24 15:15:41 +0100

    Fixed issue #2 #3 date&initial GUI

--- test_folder_exp.py
+++ test_folder_exp.py
@@ -3,3 +3,7 @@ import pandas as pd
 
 df = pd.read_excel("file.xlsx")
 print(df.head(10))
+
+# Iterate through each row and print the row data
+for index, row in df.iterrows():
+    print(f"Index: {index}, Name: {row['File Names']}")
