56b6a0f56916c20d81ce8ab28d0120e92f875437
zjerinjames <zjerinjames@gmail.com>
2023-12-27 16:32:49 +0100

    Deployed and tested after fixing Issue #1, .. , #3

--- test_folder_exp.py
+++
@@ -1,9 +0,0 @@
-# script to open excelm sheet created
-import pandas as pd
-
-df = pd.read_excel("file.xlsx")
-print(df.head(10))
-
-# Iterate through each row and print the row data
-for index, row in df.iterrows():
-    print(f"Index: {index}, Name: {row['File Names']}")
